14f2dec7e7cf6f8e47fffab416853bbedeb2dc00
Allemon <allemon3@gmail.com>
2023-11-05 18:09:58 +0100

    modify SecurityConfiguration should allow requests to register

---
+++ src/main/java/com/example/chatapp/config/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.chatapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("api/auth/register")
+                .permitAll();
+        return http.build();
+    }
+
+}
